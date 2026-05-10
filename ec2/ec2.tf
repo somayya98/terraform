0f15ba33662eca3b0395a1f01df5f116f6388257
somayya <somayya.adusumalli98@gmail.com>
2026-05-10 11:08:36 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,32 +1,41 @@
-resource "aws_instance" "terraform" {
-    ami = "ami-0220d79f3f480ecf5"
-    instance_type = "t3.micro"
-    vpc_security_group_ids =  [aws_security_group.allow_all.id]
-    tags = {
-        Name = "terraform"
-        Terraform = "true"
-    }
-}
+# resource "aws_instance" "terraform" {
+#     ami = "ami-0220d79f3f480ecf5"
+#     instance_type = "t3.micro"
+#     vpc_security_group_ids =  [aws_security_group.allow_all.id]
+#     tags = {
+#         Name = "terraform"
+#         Terraform = "true"
+#     }
+# }
 
-resource "aws_security_group" "allow_all" {
-  name   = "allow-all"
+# resource "aws_security_group" "allow_all" {
+#   name   = "allow-all"
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
   
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
   
+#   tags = {
+#     Name = "allow-all"
+#   }
+  
+# }
+
+resource "aws_instance" "example" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+
   tags = {
-    Name = "allow-all"
+    Name = "HelloWorld"
   }
-  
 }
